36d13d1f50237e0802af3dc7a8237e011e985601
Timothy Lebo <timrdf@gmail.com>
2013-01-21 14:02:52 -0500

    installing prizms

--- bin/install/project-user.sh
+++ bin/install/project-user.sh
@@ -10,12 +10,17 @@ fi
 
 user="$1"
 
-if [[ ! -e ~$user ]]; then
+   echo sudo cat /etc/passwd | cut -d: -f1 | grep $user
+exists=`sudo cat /etc/passwd | cut -d: -f1 | grep $user`
+
+if [[ -z $exists ]]; then
    admin="wheel" # Could be 'admin'
    echo sudo /usr/sbin/useradd $user
         sudo /usr/sbin/useradd $user
-   echo sudo /usr/sbin/usermod -g$user -G$user,$admin $user
-        sudo /usr/sbin/usermod -g$user -G$user,$admin $user
+   echo sudo /usr/sbin/usermod -g$user $user
+        sudo /usr/sbin/usermod -g$user $user
+   #echo sudo /usr/sbin/usermod -g$user -G$admin $user
+   #     sudo /usr/sbin/usermod -g$user -G$admin $user
 else
    "WARNING `basename $0`: $user already exists; not trying to add or modify." >&2
 fi

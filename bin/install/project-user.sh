a86e4195e882e643b1dcd5b22c4a728df14287f7
Timothy Lebo <timrdf@gmail.com>
2013-01-21 14:15:45 -0500

    installing prizms

--- bin/install/project-user.sh
+++ bin/install/project-user.sh
@@ -22,5 +22,5 @@ if [[ -z $exists ]]; then
    #echo sudo /usr/sbin/usermod -g$user -G$admin $user
    #     sudo /usr/sbin/usermod -g$user -G$admin $user
 else
-   echo "WARNING `basename $0`: $user already exists; not trying to add or modify." >&2
+   echo "INFO `basename $0`: $user already exists; not trying to add or modify." >&2
 fi

2b95e133342b0301a808e9fef6fdfbf269915ea5
Timothy Lebo <timrdf@gmail.com>
2013-02-09 15:41:31 -0500

    installing prizms

--- bin/install.sh
+++ bin/install.sh
@@ -751,9 +751,9 @@ pushd &> /dev/null
                   template="$PRIZMS_HOME/repos/csv2rdf4lod-automation/bin/conversion-root-stub/source/csv2rdf4lod-source-me-as-xxx.sh"
                   target="data/source/csv2rdf4lod-source-me-as-$project_user_name.sh"
                   if [[ ! -e $target ]]; then
-                     cat $template | grep -v 'export CSV2RDF4LOD_CONVERT_PERSON_URI='                 > $target
-                     echo "source `pwd`/data/source/csv2rdf4lod-source-me-for-$project_user_name.sh" >> $target
-                     echo "source `pwd`/data/source/csv2rdf4lod-source-me-credentials.sh"            >> $target
+                     cat $template | grep -v 'export CSV2RDF4LOD_CONVERT_PERSON_URI='                                              > $target
+                     echo "source `pwd | sed "s/\`whoami\`/$user/g"`/data/source/csv2rdf4lod-source-me-for-$project_user_name.sh" >> $target
+                     echo "source `pwd | sed "s/\`whoami\`/$user/g"`/data/source/csv2rdf4lod-source-me-credentials.sh"            >> $target
                      # any others to source?
                      added="$added $target"
                      echo

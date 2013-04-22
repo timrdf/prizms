30edd6e3a62c6c989c7b55f34eac69eee9e5ac40
Tim L <timrdf@gmail.com>
2013-04-22 11:06:26 -0400

    install

--- bin/install.sh
+++ bin/install.sh
@@ -2056,7 +2056,8 @@ else
                            base=$our_base_uri/~$person_user_name/
                            echo sudo bash -s components=$comps -s base-url=$base -s base-namespace=$our_base_uri -s sparql-endpoint=$our_base_uri/sparql < <(curl -sL http://lodspeakr.org/install)
                            # bash -s components=/location/./components base-url=http://lofd.tw.rpi.edu/~lebot/ base-namespace=.. sparql-endpoint=http://../sparql <  <(curl -sL htt...akr.org/install)
-                           read -p "Q: Install your Prizms LODSPeaKr development clone? [y/n] " -u 1 install_it
+                           echo
+                           read -p "Q: Install your Prizms LODSPeaKr development clone with the command above? [y/n] " -u 1 install_it
                            if [[ "$install_it" == [yY] ]]; then
                               echo sudo bash -s components=$comps -s base-url=$base -s base-namespace=$our_base_uri -s sparql-endpoint=$our_base_uri/sparql < <(curl -sL http://lodspeakr.org/install)
                            else

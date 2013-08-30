4eafb988245a2a978b8f90c2c1371fd287cd1a93
Tim L <timrdf@gmail.com>
2013-08-30 12:45:34 -0400

    enabling pr-spobal-ng

--- bin/dataset/pr-enable-dataset.sh
+++ bin/dataset/pr-enable-dataset.sh
@@ -51,7 +51,7 @@ if [[ $# -eq 0 ]]; then
       else
          enabled='not enabled'
       fi
-      echo "   $retrieve ($datasetID) is $enabled"
+      echo "   ($datasetID) is $enabled ($retrieve)"
    done
 else
    datasetID="$1"

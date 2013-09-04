ae6e539cfccb408bf61048c0389413daec8be4ff
Tim L <timrdf@gmail.com>
2013-09-04 10:36:43 -0400

    working https://github.com/timrdf/prizms/wiki/csv2rdf4lod-annotator

--- bin/install/prizms-dependency-repos.sh
+++ bin/install/prizms-dependency-repos.sh
@@ -43,7 +43,7 @@ pushd $PRIZMS_HOME/repos &> /dev/null
    if [[ ! -e semanteco-annotator-webapp.zip ]]; then
       # See https://github.com/timrdf/prizms/wiki/csv2rdf4lod-annotator
       echo "curl semanteco-annotator-webapp.zip"
-      curl 'https://orion.tw.rpi.edu/jenkins/job/semanteco-prizms-support/lastStableBuild/edu.rpi.tw.escience%24semanteco-annotator-webapp/artifact/*zip*/archive.zip'> semanteco-annotator-webapp.zip
+      curl --insecure 'https://orion.tw.rpi.edu/jenkins/job/semanteco-prizms-support/lastStableBuild/edu.rpi.tw.escience%24semanteco-annotator-webapp/artifact/*zip*/archive.zip'> semanteco-annotator-webapp.zip
    fi
    echo
 popd &> /dev/null

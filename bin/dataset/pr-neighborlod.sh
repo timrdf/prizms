291d477e36737471ac9fb230f99f235231ed4bee
Tim L <timrdf@gmail.com>
2013-09-12 08:55:44 -0400

    pr neighborlod - avoid sameness

--- bin/dataset/pr-neighborlod.sh
+++ bin/dataset/pr-neighborlod.sh
@@ -169,8 +169,7 @@ echo "INFO url       : $url"
                echo "<$uri> prov:wasAttributedTo <$domain> ."               >> automatic/$internal.ttl
             fi
          done
-         echo vsr-follow.sh -w -od automatic automatic/external.ttl --no-sameness --start-to --follow dcterms:references # automatic/external.ttl.ttl
-         exit
+         vsr-follow.sh -w -od automatic automatic/external.ttl --no-sameness --start-to --follow dcterms:references # automatic/external.ttl.ttl
       else
          echo "WARNING: CSV2RDF4LOD_BASE_URI \"$CSV2RDF4LOD_BASE_URI\" not http; skipping NeighborLOD."
       fi

b38442c14b1f803c145ddce9006738e6bbf1f87a
Tim L <timrdf@gmail.com>
2014-01-21 13:11:11 -0500

    added prov

--- src/python/prov-pingback.py
+++ src/python/prov-pingback.py
@@ -1,3 +1,10 @@
+#
+#3> <> prov:specializationOf <https://github.com/timrdf/prizms/blob/master/src/python/prov-pingback.py>;
+#3>    rdfs:seeAlso          <https://github.com/timrdf/prizms/wiki/prov-pingback>;
+#3>    rdfs:seeAlso          <https://github.com/timrdf/prizms/issues/19>;
+#3> .
+#
+
 from flask import Flask
 app = Flask(__name__)
 

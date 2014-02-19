baa1011a66152c5f072b0828c00f6c5eca5d2509
Tim L <timrdf@gmail.com>
2014-02-19 16:58:22 -0500

    exporting  CSV2RDF4LOD_HOME for sake of installer session

--- bin/install.sh
+++ bin/install.sh
@@ -2942,6 +2942,7 @@ else
             
             if [[ -z "$i_am_project_user" ]]; then  # Running as developer e.g. jsmith not loxd
                pushd data/source &> /dev/null
+                  export CSV2RDF4LOD_HOME=$PRIZMS_HOME/repos/csv2rdf4lod-automation # Shouldn't be needed in the long run; set above.
                   enabled=`$PRIZMS_HOME/bin/dataset/pr-enable-dataset.sh | grep 'is enabled at'`
                   if [[ -n "$enabled" ]]; then
                      echo

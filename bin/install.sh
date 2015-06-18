893cdba7e344255af2dc5d418ea0de321d351b4f
Tim L <timrdf@gmail.com>
2015-06-18 11:32:13 -0400

    working production user home setting https://github.com/timrdf/prizms/issues/105

--- bin/install.sh
+++ bin/install.sh
@@ -928,6 +928,7 @@ else
       echo
       echo "$div `whoami`"
       #echo "${user_home%/*}/$project_user_name ($project_user_home_flag)"
+      echo "$PRIZMS_HOME/bin/install/project-user.sh $project_user_home_flag $project_user_name"
       $PRIZMS_HOME/bin/install/project-user.sh --dryrun $project_user_home_flag $project_user_name
       read -p "Q: Create user $project_user_name? [y/n] " -u 1 install_project_user
       if [[ "$install_project_user" == [yY] ]]; then

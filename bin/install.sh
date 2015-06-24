4c43529047a50b66f4a7e031f50f65fcd826601c
Tim L <timrdf@gmail.com>
2015-06-24 13:55:06 -0400

    lodspeakr -http

--- bin/install.sh
+++ bin/install.sh
@@ -3208,7 +3208,7 @@ else
                echo "the version-controlled repository ($project_code_repository)." 
                echo
                if [[ ! -e $www/lodspeakr ]]; then
-                  lodspeakr_install='http://lodspeakr.org/install' # TODO: appending '-http' used to work.
+                  lodspeakr_install='http://lodspeakr.org/install-http' # TODO: appending '-http' used to work.
                   echo "$www/lodspeakr is not set up yet. It can be installed with the command:"
                   echo
                   echo " sudo -E bash -s base-url=$our_base_uri base-namespace=$our_base_uri sparql-endpoint=$our_base_uri/sparql chown=$lodchown < <(curl -sL $lodspeakr_install)"
